20ca1da913da03669c949ecd39c25de02edebdd3
msrhash <maisarahashari@gmail.com>
2025-07-17 01:31:50 +0800

    First Commit

---
+++ Assets/Components/Character/Cat/CatMovement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CatMovement : MonoBehaviour
+{
+    private PlayerControls controls;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private float horizontalMovement;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.3f;
+    private Vector3 originalScale;
+
+
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.CatControls.Move.performed += ctx => horizontalMovement = ctx.ReadValue<Vector2>().x;
+        controls.CatControls.Move.canceled += ctx => horizontalMovement = 0;
+
+        controls.CatControls.Jump.performed += ctx => Jump();
+    }
+
+    void OnEnable() => controls.CatControls.Enable();
+    void OnDisable() => controls.CatControls.Disable();
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        originalScale = transform.localScale;
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetBool("IsJumping", !isGrounded);
+
+        //flip character
+        if (horizontalMovement > 0.01f)
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        else if (horizontalMovement < -0.01f)
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+    }
+
+    void Jump()
+    {
+        Debug.Log("Cat trying to jump");
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
+
